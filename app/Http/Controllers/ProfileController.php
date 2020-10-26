82ee1edcae3c2470af4b1c214c161ff7f317129b
leonardooleg <leonardooleg2@gmail.com>
2020-10-27 01:43:10 +0200

    статуси юзеру

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,6 +2,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Status;
 use App\User;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
@@ -21,10 +22,11 @@ class ProfileController extends Controller
         $user = Auth::user();
         $orders= DB::table('orders')
             ->leftJoin('statuses', 'statuses.id', '=', 'orders.status')
-            ->select('orders.*', 'statuses.status_name')
+            ->select('orders.*', 'statuses.status_name', 'statuses.status_code')
             ->where('user_id', $user->id)
             ->orderBy('created_at', 'desc')
             ->get();
+        $all_statuses= Status::all()->toArray();
         $items =array();
         ////////
         $client = new \RetailCrm\ApiClient(
@@ -32,19 +34,51 @@ class ProfileController extends Controller
             'ctdh3KV0salK3A32t2I7TFTiSjem712B',
             \RetailCrm\ApiClient::V5
         );
+        ///
+        foreach ($orders as $order){
+            $cartCollection = $order->cart_data;
+            $items[] = json_decode($cartCollection, true);
+            $id_retail[]=$order->id_retailcrm;
+
+        }
 
         try {
-            $response = $client->request->ordersGet('M-2342');
+            $response = $client->request->ordersStatuses($id_retail,array());
         } catch (\RetailCrm\Exception\CurlException $e) {
             echo "Connection error: " . $e->getMessage();
         }
 
         if ($response->isSuccessful()) {
-            echo $response->order['totalSumm'];
-            // or $response['order']['totalSumm'];
-            // or
-            //    $order = $response->getOrder();
-            //    $order['totalSumm'];
+            //echo $response->orders[0]['status'];
+           foreach ($response->orders as $order_one){
+               foreach ($orders as $s_order)
+               if($s_order->id_retailcrm == $order_one["id"]){
+                   if($order_one['status']!=$s_order->status_code){
+                       foreach ($all_statuses as $one_statuses){
+                           if($order_one['status']==$one_statuses["status_code"]){
+                               $affected = DB::table('orders')
+                                   ->where('id', $s_order->id)
+                                   ->update(['status' => $one_statuses["id"]]);
+                               $i=0;
+                               foreach ($orders as $order){
+                                   if($order->id==$s_order->id){
+                                       $orders[$i]->status=$one_statuses["id"];
+                                       $orders[$i]->status_name=$one_statuses["status_name"];
+                                       $orders[$i]->status_code=$one_statuses["status_code"];
+                                   }
+                                   $i++;
+
+                               }
+
+                           }
+                       }
+
+                   }
+               }
+
+
+           }
+
         } else {
             echo sprintf(
                 "Error: [HTTP-code %s] %s",
@@ -52,11 +86,6 @@ class ProfileController extends Controller
                 $response->getErrorMsg()
             );
 
-        }
-        ///
-        foreach ($orders as $order){
-            $cartCollection = $order->cart_data;
-            $items[] = json_decode($cartCollection, true);
         }
 
         return view('profile.index', compact('user', 'orders', 'items'));
